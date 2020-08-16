e4a70a4e6030dc4cd92d13154adb05b8b9b32b87
Diem Ly <Diemrosely@gmail.com>
2020-08-16 00:55:41 -0400

    Updates

---
+++ precache-manifest.56cdc562f4140e698eefb5314051a065.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8741e380112639425ae109239bcd01",
+    "url": "/User-Directory-React/index.html"
+  },
+  {
+    "revision": "198b7355bb28a3eca126",
+    "url": "/User-Directory-React/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9de62bdb8c6f460d1eae",
+    "url": "/User-Directory-React/static/css/main.3e06c590.chunk.css"
+  },
+  {
+    "revision": "198b7355bb28a3eca126",
+    "url": "/User-Directory-React/static/js/2.f1ed650c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/User-Directory-React/static/js/2.f1ed650c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de62bdb8c6f460d1eae",
+    "url": "/User-Directory-React/static/js/main.bbfceefc.chunk.js"
+  },
+  {
+    "revision": "abafd6facb38d2f91e2e",
+    "url": "/User-Directory-React/static/js/runtime-main.a8005ced.js"
+  },
+  {
+    "revision": "4b569167da4efd6d2cf175d866429ca1",
+    "url": "/User-Directory-React/static/media/Logo.4b569167.png"
+  }
+]);
